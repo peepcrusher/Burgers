1a3b85893ae1212ffee8bb6b1ab007a83c1031e3
Sam Thompson <electronic62794@yahoo.com>
2019-04-04 17:26:20 -0700

    set up connection and server

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+
+var PORT = process.env.port || 3000;
+
+var app = express();
+
+//access to public folder
+app.use(express.static(__dirname + "/app/public/"));
+
+//body parser
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//set handlebars.
+var exphbs = require("express-handlebars");
+
+app.enging("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//require the controller for the routes
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+//start the server
+app.listen(PORT, function(){
+    //console log to make sure our server has started
+
+    console.log("Server Listening on PORT: " + PORT);
+})
